3068343059d92837ea0a90efae12a0f0a51dbcb3
jadepark <jadepark@192.168.1.28>
2021-11-07 18:55:42 -0600

    IJ CompanyLogo

---
+++ Company Logo - A2/src/CompanyLogo.java
@@ -0,0 +1,23 @@
+// This program will run print statement that form the logo of my company, Starr. (Jade Park - 5/14/2021)
+// Creating a class for my Company's logo (Starr)
+
+public class CompanyLogo {
+
+	public static void main(String[] args) {
+		
+		// Making the logo with a bunch of print statements!
+		System.out.println("    /////////       ///////////            *                 //////////      //////////   ");
+		System.out.println("   ///    ///       ///////////           ***                ///     ///     ///     ///  ");
+		System.out.println("   //     ///           ///             *******              ///      ///    ///      /// ");
+		System.out.println("   ///   ///            ///           ***********            ///     ///     ///     ///  ");
+		System.out.println("    ///                 ///    *************************     ///    ///      ///    ///   ");
+		System.out.println("     ////               ///      *********************       ///   ///       ///   ///    ");
+		System.out.println("      ////              ///        *****************         ////////        ////////     ");
+		System.out.println("       ////             ///          *************           ///   ///       ///    ///   ");
+		System.out.println("      ////              ///        *****************         ///    ///      ///     ///  ");
+		System.out.println("//   ////               ///       *****          ****        ///     ///     ///      /// " );
+		System.out.println("///////                 ///      **                 **       ///      ///    ///       ///");
+
+	}
+
+}
